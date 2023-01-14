7bd49c4049fb7de9811d21587b6f417bdb7794a2
Yurii Vydai <djurker21@gmail.com>
2023-01-14 11:48:17 +0200

    cats, mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as path from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.DB_URL || 'mongodb://adminyurii1:adminpass12@localhost:27017/nest1', {
+        directConnection: true,
+        tls: true,
+        tlsAllowInvalidHostnames: true,
+        retryWrites: false,
+        tlsCAFile: path.join(__dirname, '../rds-combined-ca-bundle.pem')
+      }),
+    CatModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
